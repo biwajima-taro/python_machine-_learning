48ee7c128c0e19fc542516b8c0acf3893b62c750
taro-biwajima <taro.biwajima@gmail.com>
2020-01-02 23:15:34 +0900

    test for naive bayse

---
+++ scratch/test/test_naive_bayes.py
@@ -0,0 +1,18 @@
+from naive_bayes import NaiveBayesClassifier, Message
+import sys
+import os
+sys.append(os.pardir)
+
+
+def test_train():
+    messages = [Message("spam rules", is_spam=True),
+                Message("ham rules", is_spam=False),
+                Message("hello ham", is_spam=False)]
+    model = NaiveBayesClassifier(k=0.5)
+    model.train(messages)
+
+    assert model.tokens == {"spam", "ham", "rules", "hello"}
+    assert model.spam_messages == 1
+    assert model.ham_messages == 2
+    assert model.token_spam_counts == {"spam": 1, "rules": 1}
+    assert model.token_ham_counts == {"ham": 2, "rules": 1, "hello": 1}

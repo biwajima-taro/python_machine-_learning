6a78bedf0b0d65883f4b98299f4e1ca3c14a7b45
owari-taro <58625526+owari-taro@users.noreply.github.com>
2020-06-28 22:38:41 +0900

    XOR

--- deep_learning_from_scratch/perceptron.py
+++ deep_learning_from_scratch/perceptron.py
@@ -1,4 +1,7 @@
-import numphy as np
+import numpy as np
+from typing import List
+
+
 def AND_(x1: int, x2: int, w1: float = 0.5, w2: float = 0.5, theta: float = 0.7) -> int:
     """
     ANT gate implemented by perceptron
@@ -27,11 +30,28 @@ def AND_(x1: int, x2: int, w1: float = 0.5, w2: float = 0.5, theta: float = 0.7)
         return 1
     return 0
 
-def AND(x1:int,x2:int,weight:List=[0.5,0.5],threshold:float=0.5)->int:
-    x=np.array([x1,x2])
-    w=np.array(weight)
-    
-    tmp=np.sum(x*w)-threshold]
-    if tmp>0:
+
+def AND(x1: int, x2: int, weight: List = [0.5, 0.5],
+        threshold: float = 0.7) -> int:
+    x = np.array([x1, x2])
+    w = np.array(weight)
+
+    tmp = np.sum(x*w)-threshold
+    if tmp > 0:
         return 1
     return 0
+
+
+def NAND():
+    NotImplemented
+
+
+def OR():
+    NotImplemented
+
+
+def XOR(x1: int, x2: int) -> int:
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y

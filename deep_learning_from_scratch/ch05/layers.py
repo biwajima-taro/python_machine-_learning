814ec8da902d2320ecffbb8e8999cb03cb050cf9
owari-taro <58625526+owari-taro@users.noreply.github.com>
2020-07-05 21:53:08 +0900

    softmax layer

--- deep_learning_from_scratch/ch05/layers.py
+++ deep_learning_from_scratch/ch05/layers.py
@@ -1,5 +1,10 @@
 import numpy as np
 import attr
+import sys
+import os
+sys.path.appned(os.pardir)
+from ..common.functions import cross_entropy_error, soft_max
+
 
 @attr.s
 class Relu:
@@ -26,3 +31,20 @@ class Sigmoid:
 
     def backward(self, dout):
         return dout*self.out*(1-self.out)
+
+
+@attr.s
+class SoftmaxWithLoss:
+    loss = attr.ib(default=None)
+    y = attr.ib(default=None)
+    training = attr.ib(default=None)
+
+    def forward(self, x, traning):
+        self.training = training
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.training)
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.training.shape[0]
+        dx = (self.y-self.training)/batch_size

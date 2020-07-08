42ca05416d769b845b896c38a5a691f14cbf471f
owari-taro <58625526+owari-taro@users.noreply.github.com>
2020-07-09 00:04:32 +0900

    util for NLP

---
+++ deep_learning_from_scratch_2/common/util.py
@@ -0,0 +1,27 @@
+from typing import List, Dict, Tuple
+import numpy as np
+
+
+def preprocess(text: str) -> Tuple[np.ndarray, Dict, Dict]:
+    """[summary]
+
+    Args:
+        text (str): [description]
+
+    Returns:
+        Tuple[np.ndarray,Dict,Dict]: [description]
+    """
+    text = text.lower()
+    text = text.replace(".", " .")
+    words = text.split(" ")
+    word_to_id = {}
+    id_to_word = {}
+    count = 0
+    for word in words:
+        if word_to_id.get(word) is None:
+            word_to_id[word] = count
+            id_to_word[count] = word
+            count += 1
+    corpus = np.array([word_to_id[word] for word in words])
+
+    return corpus, word_to_id, id_to_word

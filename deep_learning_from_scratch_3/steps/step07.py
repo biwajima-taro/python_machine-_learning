e1384e6fd2c3552b6d5c67f875e34ae966e9e299
owari-taro <58625526+owari-taro@users.noreply.github.com>
2020-07-10 21:44:46 +0900

    step07

---
+++ deep_learning_from_scratch_3/steps/step07.py
@@ -0,0 +1,41 @@
+import attr
+from typing import List
+import numpy as np
+
+
+@attr.s
+class Variable:
+    data: np.ndarray = attr.ib()
+    grad: np.ndarray = attr.ib()
+    # how to write Function as a type hint?
+    creator = attr.ib(default=None)
+
+    def set_creator(self, func) -> None:
+        self.creator = func
+
+    def backward(self):
+        f = self.creator
+        if is not None:
+            x = f.input
+            x.grad = f.backward(self.grad)
+            x.backward()
+
+
+@attr.s
+class Function:
+    input: Varible = attr.ib(init=None)
+    output: Variable = attr.ib(init=None)
+
+    def forward(self):
+        raise NotImplemented
+
+    def backward(self):
+        raise NotImplemented
+
+    def __call__(self, input: Variable) -> Variable:
+        self.input = input
+        y = self.forward(input.data)
+        output = Variable(y)
+        ouput.set_creator(self)
+        self.output = output
+        return Variable(y)

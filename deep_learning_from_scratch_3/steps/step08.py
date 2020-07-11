b3cf88a8c654f506d004728a15b88e58c92931c1
owari-taro <58625526+owari-taro@users.noreply.github.com>
2020-07-11 21:24:54 +0900

    rewrite backward with while statement at step08

---
+++ deep_learning_from_scratch_3/steps/step08.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+import attr
+from typing import List
+import numpy as np
+
+
+@attr.s
+class Variable:
+    data: np.ndarray = attr.ib()
+    grad: np.ndarray = attr.ib(default=None)
+    creator: Function = attr.ib(default=None)
+
+    def set_creator(self, func) -> None:
+        self.creator = func
+
+    def backward(self):
+        funcs:List[Function]=[self.creator]
+        if funcs:
+            current_f=funcs.pop()
+            x,y = f.input,f.output
+            x.grad = f.backward(y.grad)
+            if x.creator:
+                funcs.append(x.creator)            
+        return x.grad
+
+
+@attr.s
+class Function:
+    input: Variable = attr.ib(init=None)
+    output: Variable = attr.ib(init=None)
+
+    def forward(self):
+        raise NotImplemented
+
+    def backward(self):
+        raise NotImplemented
+
+    def __call__(self, input: Variable) -> Variable:
+        self.input = input
+        y = self.forward(input.data)
+        output = Variable(y)
+        output.set_creator(self)
+        self.output = output
+        return output
+

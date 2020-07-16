d14a29ffeb2c2ac8b0a821b2c53405b3d2812a54
owari-taro <58625526+owari-taro@users.noreply.github.com>
2020-07-16 23:54:41 +0900

    transpose

--- deep_learning_from_scratch_3/dezero/functions.py
+++ deep_learning_from_scratch_3/dezero/functions.py
@@ -2,7 +2,19 @@ from .core_simple import Function, Variable
 import attr
 from typing import Tuple
 from .core import as_variable
+import numpy as np
 
+
+@attr.s
+class Transpose(Function):
+    def forward(self,x:np.ndarray)->np.ndarray:
+        np.transpose(x)
+
+    def backward(self,dy):
+        return transpose(dy)
+
+def transpose(x):
+    return Transpose()(x)
 
 @attr.s
 class Reshape(Function):
